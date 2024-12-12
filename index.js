66d6f07cffd675627cad04553e741412d87f494d
WHSVic <110412906+WHSVic@users.noreply.github.com>
2024-12-12 12:05:09 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors'); 
+const app = express();
+const port = 3000;
+// allow requests from anywhere
+app.use(cors());
+// define our api route
+// add, sub, div, mult, power, root, square
+app.get('/add', (req, res) => {
+ const a = parseFloat(req.query.a);
+ const b = parseFloat(req.query.b);
+ const result = a+b ;
+ return res.json({ result: result});
+}); 
+app.listen(port,() => {console.log('Server running on'+port)});
